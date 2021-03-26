456df3fee06ed8aa1ea1fcc129142f5e3aa327fe
k-takomal <foxinn009@gmail.com>
2021-03-27 06:44:10 +0900

    メイン画面のプルダウン表示の変更

--- app/javascript/pull_down.js
+++ app/javascript/pull_down.js
@@ -1,10 +1,29 @@
 (()=>{
+
    function pullDown(){
 
     const pullDownButton = document.getElementById("list");
     const pullDownParents = document.getElementById("pull_down");
 
-    
+    //マウスを載せるとプルダウンメニュー表示
+    pullDownButton.addEventListener("mouseover",function(){
+        pullDownParents.setAttribute("style","display:block;")
+      });
+    pullDownParents.addEventListener("mouseover",function(){
+        pullDownParents.setAttribute("style","display:block;")
+      });
+
+
+    //マウスを載せるとプルダウンメニュー非表示
+    pullDownButton.addEventListener("mouseout",function(){
+      pullDownParents.setAttribute("style","display:none;")
+    });
+    pullDownParents.addEventListener("mouseout",function(){
+      pullDownParents.setAttribute("style","display:none;")
+    });
+
+
+  //クリックするとプルダウンメニュー表示・非表示
     pullDownButton.addEventListener("click",function(){
 
       if(pullDownParents.getAttribute("Style")=="display:none;"){
@@ -14,6 +33,6 @@
       
     });
   };
-  // window.addEventListener("load",pullDown);
   setInterval(pullDown,500);
+  // window.addEventListener("load",pullDown);
 })();
---
+++ app/javascript/reload.js
@@ -0,0 +1,7 @@
+
+  window.onpageshow = function(event) {
+    if(event.persisted){
+         window.location.reload();
+         console.log("ok")
+     }
+};
